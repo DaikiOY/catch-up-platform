7210e0f7f5a448c1368f408e7d51360f09c89d0b
alberto sanchez <alberto.sanchez@reinamadre.mx>
2024-10-21 20:01:40 -0500

    feat(favorite-source): add FavoriteSource implementation

---
+++ CatchUpPlatform.API/News/Infrastructure/Repositories/FavoriteSourceRepository.cs
@@ -0,0 +1,23 @@
+﻿using CatchUpPlatform.API.News.Domain.Model.Aggregates;
+using CatchUpPlatform.API.News.Domain.Repositories;
+using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using CatchUpPlatform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace CatchUpPlatform.API.News.Infrastructure.Repositories
+{
+    public class FavoriteSourceRepository(AppDbContext context) : BaseRepository<FavoriteSource>(context), IFavoriteSourceRepository
+    {
+        /// <inheritdoc />
+        public async Task<IEnumerable<FavoriteSource>> FindByNewsApiKeyAsync(string newsApiKey)
+        {
+            return await context.Set<FavoriteSource>().Where(f => f.NewsApiKey == newsApiKey).ToListAsync();
+        }
+
+        /// <inheritdoc />
+        public async Task<FavoriteSource?> FindByNewsApiKeyAndSourceIdAsync(string newsApiKey, string sourceId)
+        {
+            return await context.Set<FavoriteSource>().FirstOrDefaultAsync(f => f.NewsApiKey == newsApiKey && f.SourceId == sourceId);
+        }
+    }
+}
